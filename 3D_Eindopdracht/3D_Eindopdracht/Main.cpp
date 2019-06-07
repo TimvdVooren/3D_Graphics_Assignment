c3b920e7a5d8681f004606d230f990c4fd31a604
Unknown <tvooren1@avans.nl>
2019-06-07 12:19:08 +0200

    [Added] freeglut and World.cpp

---
+++ 3D_Eindopdracht/3D_Eindopdracht/Main.cpp
@@ -0,0 +1,33 @@
+#include "GL/freeglut.h"
+
+int main(int argc, char * argv[])
+{
+	glEnable(GL_DEPTH_TEST);
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+	RECT desktop;
+	const HWND hDesktop = GetDesktopWindow();
+	GetWindowRect(hDesktop, &desktop);
+	glutInitWindowSize(desktop.right, desktop.bottom);
+
+	glutCreateWindow("Tim van der Vooren");
+
+	/*glutIdleFunc([]()
+	{
+		World::getWorld()->idle();
+		Game::getInstance()->handleEvents();
+
+	});*/
+
+	/*glutDisplayFunc([]() { World::getWorld()->display(); });
+	glutReshapeFunc([](int horizontal, int vertical) { World::getWorld()->reshape(horizontal, vertical); });
+	glutKeyboardFunc([](unsigned char key, int mouseX, int mouseY) { World::getWorld()->keyboard(key, mouseX, mouseY); });
+	glutKeyboardUpFunc([](unsigned char key, int mouseX, int mouseY) { World::getWorld()->keyboardUp(key, mouseX, mouseY); });
+	glutPassiveMotionFunc([](int mouseX, int mouseY) {World::getWorld()->mousePassiveMotion(mouseX, mouseY); });
+	glutMouseFunc([](int button, int state, int x, int y) {World::getWorld()->mouseClick(button, state, x, y); });
+*/
+
+	glutMainLoop();
+}
